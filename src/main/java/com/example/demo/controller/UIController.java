a39d93d6c33fc344b378331a146bb9f739b0443e
Panthangi <kranthi.panthangi@capgemini.com>
2020-12-30 20:26:04 -0600

    ui controller

--- src/main/java/com/example/demo/controller/UIController.java
+++ src/main/java/com/example/demo/controller/UIController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UIController {
 
-	@GetMapping("/")
+	@GetMapping("/ui")
 	public String getUI() {
 		return "hello from ui";
 		
